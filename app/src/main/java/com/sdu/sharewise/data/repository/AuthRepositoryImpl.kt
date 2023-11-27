84704851a29f8330ab89487c5cb66b45546b033f
AlexGlering <afglering@gmail.com>
2023-11-27 19:06:18 +0000

    Login, register, createGroup, createUser, updateUserName, updateUserEmail, updateUserPhone custom performance tracing

--- app/src/main/java/com/sdu/sharewise/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/sdu/sharewise/data/repository/AuthRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.sdu.sharewise.data.repository
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.perf.FirebasePerformance
 import com.sdu.sharewise.data.Resource
 import com.sdu.sharewise.data.utils.await
 import javax.inject.Inject
@@ -14,27 +15,37 @@ class AuthRepositoryImpl @Inject constructor(
     override val currentUser: FirebaseUser?
         get() = firebaseAuth.currentUser
 
+
     override suspend fun login(email: String, password: String): Resource<FirebaseUser> {
+        val trace = FirebasePerformance.getInstance().newTrace("login_trace")
+        trace.start() //tracing login
         return try {
             val result = firebaseAuth.signInWithEmailAndPassword(email, password).await()
+            trace.stop()
             Resource.Success(result.user!!)
         } catch (e: Exception) {
+            trace.stop()
             e.printStackTrace()
             Resource.Failure(e)
         }
     }
+
 
     override suspend fun register(
         name: String,
         email: String,
         password: String
     ): Resource<FirebaseUser> {
+        val trace = FirebasePerformance.getInstance().newTrace("register_trace")
+        trace.start() //tracing user registering
         return try {
             val result = firebaseAuth.createUserWithEmailAndPassword(email, password).await()
             result?.user?.updateProfile(UserProfileChangeRequest.Builder().setDisplayName(name).build())?.await()
             currentUser?.let { userRepository.createUser(it.uid, name, email, phone = "") }
+            trace.stop()
             Resource.Success(result.user!!)
         } catch (e: Exception) {
+            trace.stop()
             e.printStackTrace()
             Resource.Failure(e)
         }
--- app/src/main/java/com/sdu/sharewise/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/sdu/sharewise/data/repository/UserRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.sdu.sharewise.data.repository
 
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.perf.FirebasePerformance
 import com.sdu.sharewise.data.Resource
 import com.sdu.sharewise.data.model.User
 import com.sdu.sharewise.data.utils.await
@@ -18,21 +19,29 @@ class UserRepositoryImpl @Inject constructor(
         email: String,
         phone: String
     ): Resource<User> {
+        val trace = FirebasePerformance.getInstance().newTrace("createUser_trace")
+        trace.start()
         return try {
             val user = User(uuid = uuid, name = name, email = email, phone = phone)
             firebaseDB.getReference("Users").child(uuid).setValue(user).await()
+            trace.stop()
             Resource.Success(user)
         } catch (e: Exception) {
+            trace.stop()
             e.printStackTrace()
             Resource.Failure(e)
         }
     }
 
     override suspend fun updateUserName(uuid: String, name: String): Resource<String> {
+        val trace = FirebasePerformance.getInstance().newTrace("updateUserName_trace")
+        trace.start()
         return try {
             firebaseDB.getReference("Users").child(uuid).child("name").setValue(name).await()
+            trace.stop()
             Resource.Success(name)
         } catch (e: Exception) {
+            trace.stop()
             e.printStackTrace()
             Resource.Failure(e)
         }
@@ -42,10 +51,14 @@ class UserRepositoryImpl @Inject constructor(
         uuid: String,
         email: String
     ): Resource<String> {
+        val trace = FirebasePerformance.getInstance().newTrace("updateUserEmail_trace")
+        trace.start()
         return try {
             firebaseDB.getReference("Users").child(uuid).child("email").setValue(email).await()
+            trace.stop()
             Resource.Success(email)
         } catch (e: Exception) {
+            trace.stop()
             e.printStackTrace()
             Resource.Failure(e)
         }
@@ -55,10 +68,14 @@ class UserRepositoryImpl @Inject constructor(
         uuid: String,
         phone: String
     ): Resource<String> {
+        val trace = FirebasePerformance.getInstance().newTrace("updateUserPhone_trace")
+        trace.start()
         return try {
             firebaseDB.getReference("Users").child(uuid).child("phone").setValue(phone).await()
+            trace.stop()
             Resource.Success(phone)
         } catch (e: Exception) {
+            trace.stop()
             e.printStackTrace()
             Resource.Failure(e)
         }
