a5fd865f6351124d68f1245169e3695a04bae7f3
Storm311 <stormfrederik@gmail.com>
2024-01-06 18:07:06 +0100

    Created database files for GroupExpenses

---
+++ app/src/main/java/com/sdu/sharewise/data/repository/GroupExpenseRepository.kt
@@ -0,0 +1,38 @@
+package com.sdu.sharewise.data.repository
+
+import com.google.firebase.database.FirebaseDatabase
+import com.sdu.sharewise.data.Resource
+import com.sdu.sharewise.data.model.Expense
+
+
+interface GroupExpenseRepository {
+    val database: FirebaseDatabase?
+
+    suspend fun createGroupExpense(
+        expenseUid: String,
+        groupUid: String,
+        amount: Float,
+        expenseCreator: String,
+        expensePayer: String,
+        paid: Boolean,
+    ): Resource<Expense>
+
+    suspend fun removeExpense(
+        expenseUid: String,
+        groupUid: String,
+        paid: Boolean
+    )
+
+    suspend fun payExpense(
+        expenseId: Int,
+        expensePayer: String,
+        amount: Float,
+        paid: Boolean
+    )
+
+    suspend fun totalExpenses(
+        groupUid: String,
+        amount: Float,
+    )
+
+}
