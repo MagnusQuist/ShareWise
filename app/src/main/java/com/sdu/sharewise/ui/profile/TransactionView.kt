d57ca30e04fd693ff90a3d2d25b72df619d24615
Rasmus Helleberg Eriksen <raeri21@student.sdu.dk>
2024-01-07 19:47:29 +0100

    Transaction view init

---
+++ app/src/main/java/com/sdu/sharewise/ui/profile/TransactionView.kt
@@ -0,0 +1,13 @@
+package com.sdu.sharewise.ui.profile
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+
+@Composable
+fun TransactionView(
+    viewModel: ProfileViewModel = hiltViewModel(),
+    navController: NavHostController
+) {
+
+}
