2ddaaef1d5ec3c8ed86ec02b7a7258a8084952e1
Magnus Quist <cloudquist@gmail.com>
2024-01-08 15:39:02 +0100

    expense notification

--- app/src/main/java/com/sdu/sharewise/data/repository/GroupExpenseRepositoryImpl.kt
+++ app/src/main/java/com/sdu/sharewise/data/repository/GroupExpenseRepositoryImpl.kt
@@ -8,13 +8,15 @@ import com.google.firebase.perf.FirebasePerformance
 import com.sdu.sharewise.data.Resource
 import com.sdu.sharewise.data.model.Expense
 import com.sdu.sharewise.data.utils.await
+import com.sdu.sharewise.services.NotificationSenderService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlinx.coroutines.CompletableDeferred
 import javax.inject.Inject
 
 class GroupExpenseRepositoryImpl @Inject constructor(
-    private val firebaseDB: FirebaseDatabase
+    private val firebaseDB: FirebaseDatabase,
+    private val userRepository: UserRepository,
 ) : GroupExpenseRepository {
     override val database: FirebaseDatabase?
         get() = firebaseDB
@@ -36,6 +38,16 @@ class GroupExpenseRepositoryImpl @Inject constructor(
             val expense = Expense(uid = expenseUid, groupUid = groupUid, amount = amount, expenseDesc = expenseDesc, expenseCreator = expenseCreator, expensePayer = expensePayer, paid = paid, time = time)
             firebaseDB.getReference("GroupExpenses").child(expenseUid).setValue(expense).await()
             trace.stop()
+            val tokens: ArrayList<String> = ArrayList()
+            userRepository.getTokenFromUuid(expensePayer) {token ->
+                if (token != null) { tokens.add(token) }
+            }
+
+            NotificationSenderService.sendNotification(
+                tokens,
+                "New expense",
+                "You owe $amount DKK"
+            )
             Resource.Success(expense)
         } catch (e: Exception) {
             trace.stop()
